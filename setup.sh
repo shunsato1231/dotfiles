fc255d66399bac2465f1fa01df9ed1e5cbf98b97
shun-sato <shun.sato@northdetail.co.jp>
2020-11-12 13:27:16 +0900

    ファイルが存在しない場合作成

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -l
 cd ~/
+if [ ! -e ~/git-prompt.sh ]; then
+  touch ~/git-prompt.sh
+fi
+
+if [ ! -e ~/git-completion.bash ]; then
+  touch ~/git-completion.bash
+fi
+
+if [ ! -e ~/.bash_profile ]; then
+  touch ~/.bash_profile
+fi
+
 ln -is ~/dotfiles/git-prompt.sh .
 ln -is ~/dotfiles/git-completion.bash .
 ln -is ~/dotfiles/.bash_profile .
