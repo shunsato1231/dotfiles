0e5de263fe49f636ad1dc2c31c2d2c2b946be3bf
shun-sato <shun.sato@northdetail.co.jp>
2020-11-12 13:34:11 +0900

    .bashrc追加

--- setup.sh
+++ setup.sh
@@ -8,12 +8,17 @@ if [ ! -e ~/git-completion.bash ]; then
   touch ~/git-completion.bash
 fi
 
+if [ ! -e ~/.bashrc ]; then
+  touch ~/.bashrc
+fi
+
 if [ ! -e ~/.bash_profile ]; then
   touch ~/.bash_profile
 fi
 
 ln -is ~/dotfiles/git-prompt.sh .
 ln -is ~/dotfiles/git-completion.bash .
+ln -is ~/dotfiles/.bash_rc .
 ln -is ~/dotfiles/.bash_profile .
 
 # vscode
